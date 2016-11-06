a5521f81620aa837133fa659f53776be71269b10
Simon Colmer <simon_colmer@hotmail.com>
2016-11-06 16:43:25 +0000

    Added test board script

---
+++ test-board.ts
@@ -0,0 +1,20 @@
+/// <reference path="./typings/index.d.ts"/>
+var five = require('johnny-five');
+var board, led;
+
+console.log("Get a new board");
+
+board = new five.Board({port: "/dev/ttyAMA0"});
+
+board.on("ready", function() {
+
+    console.log("J5 Firing LEDs");
+
+    var val = 0;
+
+    this.pinMode(13, 1);
+    this.loop(1000, function() {
+        this.digitalWrite(13, (val = val ? 0 : 1) );
+    })
+
+});
