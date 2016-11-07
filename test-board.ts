ff5af11b784f9f20c097e9ef7890ff1c1591aa52
Simon Colmer <simon_colmer@hotmail.com>
2016-11-07 23:20:49 +0000

    Added code in for raw arm playing

--- test-board.ts
+++ test-board.ts
@@ -1,20 +1,47 @@
 /// <reference path="./typings/index.d.ts"/>
-var five = require('johnny-five');
-var board, led;
+import async = require("async");
+import j5 = require("johnny-five");
+var board;
 
-console.log("Get a new board");
-
-board = new five.Board({port: "/dev/ttyAMA0"});
+board = new j5.Board({port: "/dev/ttyAMA0"});
 
 board.on("ready", function() {
+  var arm1 = new j5.Servo({
+      pin: 14,
+      range: [15, 180], //TODO: Work out these values
+      center: true
+    });
 
-    console.log("J5 Firing LEDs");
+  var arm2 = new j5.Servo({
+      pin: 15,
+      range: [20, 150], //TODO: Work out these values
+      center: true
+    });
 
-    var val = 0;
-
-    this.pinMode(13, 1);
-    this.loop(1000, function() {
-        this.digitalWrite(13, (val = val ? 0 : 1) );
-    })
+  async.forever((foreverCallback : () => void) => {
+    async.waterfall([
+      function(callback) {
+        arm1.min();
+        setTimeout(callback, 1000);
+      },
+      function(callback) {
+        arm2.min();
+        setTimeout(callback, 1000);
+      },
+      function(callback) {
+        arm1.max();
+        setTimeout(callback, 1000);
+      },
+      function(callback) {
+        arm2.max();
+        setTimeout(callback, 1000);
+      },
+      function(callback) {
+        arm2.center();
+        arm1.center();
+        setTimeout(callback, 1000);
+      }
+    ], () => foreverCallback());
+  }, () => {});
 
 });
