3b40ea9b75e4d6cbfd677a85638a05d5e4feab94
Simon Colmer <simon_colmer@hotmail.com>
2016-11-15 21:41:20 +0000

    Fixed build

--- sensor-test.js
+++ sensor-test.js
@@ -2,9 +2,9 @@
 var j5 = require("johnny-five");
 var board = new j5.Board({ port: "/dev/ttyAMA0" });
 board.on("ready", function () {
-    var proximity = new j5.Proximity({
+    var proximity = new j5.IR.Proximity({
         controller: "GP2Y0A21YK",
-        pin: "A0"
+        pin: "A4"
     });
     proximity.on("data", function () {
         console.log("inches: ", this.inches);
--- sensor-test.ts
+++ sensor-test.ts
@@ -4,9 +4,9 @@ import j5 = require("johnny-five");
 var board = new j5.Board({port: "/dev/ttyAMA0"});
 
 board.on("ready", function() {  
-    var proximity = new j5.Proximity({
+    var proximity = new j5.IR.Proximity({
       controller: "GP2Y0A21YK",
-      pin: "A0"
+      pin: "A4"
     });
 
     proximity.on("data", function() {
