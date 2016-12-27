f91b3bb6f52d8de832405c33ac79316922fb3489
Simon Colmer <simon@colmer.me>
2016-12-27 21:22:55 +0000

    Added adafruit test

---
+++ adafruit-test.ts
@@ -0,0 +1,19 @@
+/// <reference path="./typings/index.d.ts"/>
+import five = require("johnny-five");
+
+//var boardOptions = new BoardOptions(; // connect over serial
+var board = new five.Board({"port": "/dev/ttyAMA0"});
+
+
+board.on("ready", function() {
+
+  var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
+  var motor1 = new five.Motor(configs.M1);
+
+  // Start the motor at maximum speed
+  motor1.start(150);
+  
+  setTimeout(function() {motor1.stop();}, 5000);
+  
+
+});
