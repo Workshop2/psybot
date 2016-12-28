ae2b389647e8ffe07035ee0a632a784eafd82f74
Simon Colmer <simon_colmer@hotmail.com>
2016-12-28 14:06:25 +0000

    Added in reversing into motor test

--- motor-test.ts
+++ motor-test.ts
@@ -8,14 +8,32 @@ var psybot = new psybotLib.Psybot(config.settings.usbConnection);
 
 psybot.board.on("ready", function() {
   this.repl.inject({psybot: psybot});
+  psybot.motors.speed = 200;
 
   async.waterfall([
     function(callback) {
-        psybot.motors.speed = 200;
         psybot.motors.forward(callback);
     },
     function(callback) {
-      setTimeout(callback, 30000);
+      setTimeout(callback, 5000);
+    },
+    function(callback) {
+      psybot.motors.reverse(callback);
+    },
+    function(callback) {
+      setTimeout(callback, 5000);
+    },
+    function(callback) {
+        psybot.motors.forward(callback);
+    },
+    function(callback) {
+      setTimeout(callback, 5000);
+    },
+    function(callback) {
+      psybot.motors.reverse(callback);
+    },
+    function(callback) {
+      setTimeout(callback, 5000);
     },
     function(callback) {
       psybot.motors.brake(callback);
