e9b49d22ffa813a36c5193a80dc055d8318fc3f0
Simon Colmer <simon_colmer@hotmail.com>
2016-12-28 19:35:53 +0000

    Forgot to build TSC

--- sensor-test.js
+++ sensor-test.js
@@ -4,7 +4,7 @@ var board = new j5.Board({ port: "/dev/ttyAMA0" });
 board.on("ready", function () {
     var proximity = new j5.IR.Proximity({
         controller: "GP2Y0A21YK",
-        pin: "A4"
+        pin: "A0"
     });
     proximity.on("data", function () {
         console.log("inches: ", this.inches);
