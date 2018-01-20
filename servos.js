8ac2fdfe46e0ba1b23a25db786ac6d3f4fcc1e31
Simon Colmer <simon@colmer.me>
2018-01-20 18:45:14 +0000

    Servo just centers

--- servos.js
+++ servos.js
@@ -1,29 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var async = require("async");
 var psybotLib = require("./psybot-lib/psybot");
 var config = require('./config/config');
 var psybot = new psybotLib.Psybot(config.settings.usbConnection);
 psybot.board.on("ready", function () {
     this.repl.inject({ psybot: psybot });
-    async.forever(function (foreverCallback) {
-        async.waterfall([
-            function (callback) {
-                psybot.frontArm.faceUp(callback);
-            },
-            function (callback) {
-                psybot.frontArm.faceDown(callback);
-            },
-            function (callback) {
-                psybot.frontArm.faceLeft(callback);
-            },
-            function (callback) {
-                psybot.frontArm.faceRight(callback);
-            },
-            function (callback) {
-                psybot.frontArm.center(callback);
-            }
-        ], function () { return foreverCallback(); });
-    }, function () { });
+    psybot.frontArm.center();
+    // async.forever((foreverCallback : () => void) => {
+    //   async.waterfall([
+    //     function(callback) {
+    //       psybot.frontArm.faceUp(callback);
+    //     },
+    //     function(callback) {
+    //       psybot.frontArm.faceDown(callback);
+    //     },
+    //     function(callback) {
+    //       psybot.frontArm.faceLeft(callback);
+    //     },
+    //     function(callback) {
+    //       psybot.frontArm.faceRight(callback);
+    //     },
+    //     function(callback) {
+    //       psybot.frontArm.center(callback);
+    //     }
+    //   ], () => foreverCallback());
+    // }, () => {});
 });
 //# sourceMappingURL=servos.js.map
--- servos.ts
+++ servos.ts
@@ -8,23 +8,25 @@ var psybot = new psybotLib.Psybot(config.settings.usbConnection);
 psybot.board.on("ready", function() {
   this.repl.inject({psybot: psybot});
 
-  async.forever((foreverCallback : () => void) => {
-    async.waterfall([
-      function(callback) {
-        psybot.frontArm.faceUp(callback);
-      },
-      function(callback) {
-        psybot.frontArm.faceDown(callback);
-      },
-      function(callback) {
-        psybot.frontArm.faceLeft(callback);
-      },
-      function(callback) {
-        psybot.frontArm.faceRight(callback);
-      },
-      function(callback) {
-        psybot.frontArm.center(callback);
-      }
-    ], () => foreverCallback());
-  }, () => {});
+  psybot.frontArm.center();
+
+  // async.forever((foreverCallback : () => void) => {
+  //   async.waterfall([
+  //     function(callback) {
+  //       psybot.frontArm.faceUp(callback);
+  //     },
+  //     function(callback) {
+  //       psybot.frontArm.faceDown(callback);
+  //     },
+  //     function(callback) {
+  //       psybot.frontArm.faceLeft(callback);
+  //     },
+  //     function(callback) {
+  //       psybot.frontArm.faceRight(callback);
+  //     },
+  //     function(callback) {
+  //       psybot.frontArm.center(callback);
+  //     }
+  //   ], () => foreverCallback());
+  // }, () => {});
 });
