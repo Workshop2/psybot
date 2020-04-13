fd366640473b5c89eeb553d247ecb2b632125fea
workshop2 <simon@colmer.me>
2020-04-13 13:50:09 +0100

    Remove JS files and build to output directory

--- psybot-lib/components/sonar.ts
+++ psybot-lib/components/sonar.ts
@@ -2,24 +2,23 @@ import j5 = require("johnny-five");
 import { ProximityData } from "johnny-five";
 
 export class Sonar {
-  private minimumDistance : number = 20;
-  private sonar : j5.Proximity;
-  private _obstacleDetected? : () => void;
+  private minimumDistance: number = 20;
+  private sonar: j5.Proximity;
+  private _obstacleDetected?: () => void;
 
-  constructor(sonarOptions : j5.ProximityOption) {
+  constructor(sonarOptions: j5.ProximityOption) {
     this.sonar = new j5.Proximity(sonarOptions);
-    this.sonar.on("data", (proximityData : ProximityData) => {
-      //console.log("data");
-      if(this._obstacleDetected != null) {
-        if(proximityData.cm < this.minimumDistance) {
+    this.sonar.on("data", (proximityData: ProximityData) => {
+
+      if (this._obstacleDetected != null) {
+        if (proximityData.cm < this.minimumDistance) {
           this._obstacleDetected();
         }
       }
     });
   }
 
-  public setObstacleDetectedCallback(obstacleDetected : () => void) : void {
-    console.log("settings");
+  public setObstacleDetectedCallback(obstacleDetected: () => void): void {
     this._obstacleDetected = obstacleDetected;
   }
 }
--- psybot-lib/delay.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
-//# sourceMappingURL=delay.js.map
