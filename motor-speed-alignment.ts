3890daed4d93b98bc5e52db6d3ea0a1baf503b05
workshop2 <simon@colmer.me>
2020-04-18 15:40:52 +0100

    Test program to measure speed of motors

---
+++ motor-speed-alignment.ts
@@ -0,0 +1,24 @@
+import { Psybot } from "./psybot-lib/psybot";
+var config = require('./config/config');
+import delay from "./psybot-lib/delay";
+
+const run = async () => {
+  var psybot = await Psybot.Create(config.settings.usbConnection);
+  await psybot.motors.brakeAsync();
+  
+  console.log("speed", psybot.motors.speed);
+
+  console.log("3")
+  await delay(1000);
+  console.log("2")
+  await delay(1000);
+  console.log("1")
+  await delay(1000);
+
+  await psybot.motors.forwardAsync();
+  await delay(12000);
+
+  await psybot.motors.brakeAsync();
+}
+
+run();
