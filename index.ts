28b699f402400b5fae35bc5b7551b7b213ef29e3
workshop2 <simon@colmer.me>
2020-05-03 19:16:59 +0100

    Remove pre-post brackets

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Psybot } from "./psybot-lib/psybot";
 import { PsybotActor } from "./psybot-actor";
-require('console-stamp')(console, '[HH:MM:ss.l]');
+require('console-stamp')(console, 'HH:MM:ss.l');
 var config = require("./config/config");
 
 const run = async () => {
