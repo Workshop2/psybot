ead9c5edd1eb09ff1999d472062034bb73191ff4
workshop2 <simon@colmer.me>
2020-09-12 12:33:19 +0100

    Show bearing

--- experiments/imu-test.ts
+++ experiments/imu-test.ts
@@ -14,47 +14,53 @@ const run = async () => {
     });
     console.log("Done")
     console.log("imu", imu)
-  
+
     // imu.on("change", function() {
     //   console.log("change", this);
     // });
-  
-    imu.on("calibrated", function() {
+
+    imu.on("calibrated", function () {
       console.log("calibrated :)");
     });
-  
-    imu.on("data", function(err, data) {
+
+    imu.on("data", function (err, data) {
       //console.log("data", this);
-      console.log("Accelerometer: %d, %d, %d", this.accelerometer.x, this.accelerometer.z, this.accelerometer.z);
-      console.log("Gyro: %d, %d, %d", this.gyro.x, this.gyro.z, this.gyro.z);
-      console.log("Temperature: %d", this.temperature.celsius);
+      // console.log("Accelerometer: %d, %d, %d", this.accelerometer.x, this.accelerometer.z, this.accelerometer.z);
+      // console.log("Gyro: %d, %d, %d", this.gyro.x, this.gyro.z, this.gyro.z);
+      // console.log("Temperature: %d", this.temperature.celsius);
 
       // console.log("Orientation:", this.orientation);
-      console.log("orientation");
-      console.log("  w            : ", imu.orientation?.quarternion?.w);
-      console.log("  x            : ", imu.orientation?.quarternion?.x);
-      console.log("  y            : ", imu.orientation?.quarternion?.y);
-      console.log("  z            : ", imu.orientation?.quarternion?.z);
-  
-      console.log("  heading      : ", imu.orientation?.euler?.heading);
-      console.log("  roll         : ", imu.orientation?.euler?.roll);
-      console.log("  pitch        : ", imu.orientation?.euler?.pitch);
+      // console.log("orientation");
+      // console.log("  w            : ", imu.orientation?.quarternion?.w);
+      // console.log("  x            : ", imu.orientation?.quarternion?.x);
+      // console.log("  y            : ", imu.orientation?.quarternion?.y);
+      // console.log("  z            : ", imu.orientation?.quarternion?.z);
+
+      // console.log("  heading      : ", imu.orientation?.euler?.heading);
+      // console.log("  roll         : ", imu.orientation?.euler?.roll);
+      // console.log("  pitch        : ", imu.orientation?.euler?.pitch);
+
+      console.log("magnetometer");
+      console.log("  heading : ", Math.floor(imu?.magnetometer?.heading));
+      console.log("  bearing : ", imu?.magnetometer?.bearing?.name);
+      console.log("--------------------------------------");
     });
+
 
     // imu.orientation.on("change", () => {
 
     //   console.log("orientation");
     //   console.log("  w            : ", imu.quarternion.w);
     //   console.log("  x            : ", imu.quarternion.x);
-    //   console.log("  y            : ", imu.quarternion.y);
+    //   console.log("  y            : ", imu.quarternion.y); 
     //   console.log("  z            : ", imu.quarternion.z);
-  
+
     //   console.log("  heading      : ", imu.euler.heading);
     //   console.log("  roll         : ", imu.euler.roll);
     //   console.log("  pitch        : ", imu.euler.pitch);
-  
+
     //   console.log("--------------------------------------");
-  
+
     // });
   });
 
