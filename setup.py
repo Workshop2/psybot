c0567ed25e44f08ab9f84af6721ff1b7e9509ced
Simon Colmer <simon@colmer.me>
2021-07-06 19:35:08 +0100

    Tweak script to support bit banged sensor

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ with open('/boot/config.txt', 'r') as file:
   
 # https://di-marco.net/blog/it/2020-04-18-tips-disabling_bluetooth_on_raspberry_pi/
 # disable bluetooth, and switch to use alt i2c for compass
-filedata += "\ndtoverlay=disable-bt,i2c-gpio,bus=3,i2c_gpio_sda=02,i2c_gpio_scl=03"
+filedata += "\ndtoverlay=disable-bt"
+filedata += "\ndtoverlay=i2c-gpio,bus=3,i2c_gpio_sda=02,i2c_gpio_scl=03"
 filedata += "\nhdmi_force_hotplug=1"
 filedata += "\nhdmi_ignore_edid=0xa5000080" 	 
 filedata += "\nhdmi_group=2"
